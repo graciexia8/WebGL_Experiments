eb8c9a3eddade246eba66b085ab42a080df9d7ea
Gracie Xia <graciexia@gmail.com>
2020-04-26 20:57:35 -0400

    added correct colors given by youtube tutorial

--- web_page_one/webgl_program_one/model.js
+++ web_page_one/webgl_program_one/model.js
@@ -1,6 +1,7 @@
-window.Triangle = function (vertices) {
+window.Triangle = function (vertices, colors) {
     var self = this;
     self.vertices = vertices;
+    self.colors = colors;
   };
 
   window.SimpleModel = function (name) {
@@ -13,11 +14,13 @@ window.Triangle = function (vertices) {
     var vertices, triangle1
   
     // Vertex data
-    vertices = [ [0.0, 0.75], [-0.5, -0.5], [0.5, -0.5 ]];
+    vertices = [ [0.0, 1.0], [-0.5, -0.5], [0.5, -0.5] ];
   
     // Create 4 triangles
-    triangle1 = new Triangle([vertices[0], vertices[1], vertices[2]]);
-  
+    colors = [[ 1.0, 1.0, 0.0],[0.7, 0.0, 1.0], [0.1, 1.0, 0.6]];
+    triangle1 = new Triangle([vertices[0], vertices[1], vertices[2]], colors);
+    
+    
     // Create a model that is a triangle
     var model = new SimpleModel("triangle");
     model.triangle = triangle1;
