c3fd8727405052c7fda387f03f01c1c17d4645a1
Gracie Xia <graciexia@gmail.com>
2020-06-05 11:34:40 -0400

    reorganized flowerpot code structure

--- flower_pot/Javascript/app.js
+++ flower_pot/Javascript/app.js
@@ -1,23 +1,25 @@
+"use strict";
+
 window.initDemo = function () {
-  loadTextResource("./shader.vs.glsl", function (vsErr, vsText) {
+  loadTextResource("../flower_pot/Shaders/shader.vs.glsl", function (vsErr, vsText) {
     if (vsErr) {
       alert("Fatal Error: Getting vertex shader");
       console.error(vsErr);
     }
     else {
-      loadTextResource("./shader.fs.glsl", function (fsErr, fsText) {
+      loadTextResource("../flower_pot/Shaders/shader.fs.glsl", function (fsErr, fsText) {
         if (fsErr) {
           alert("Fatal Error: Getting fragment shader");
           console.error(fsErr);
         }
         else {
-          loadJSONResource("./flower_pot.json", function (modelErr, modelText){
+          loadJSONResource("../flower_pot/Model/flower_pot.json", function (modelErr, modelText){
             if (modelErr) {
               alert("Fatal Error: Getting model json");
               console.error(modelErr);
             }
             else {
-              runDemo(vsText, fsText, modelText);
+              runDemo(vsText, fsText, modelText); // Successfully load vertex shader, fragment shader and model data before rendering.
             }
           });
         
@@ -31,7 +33,7 @@ window.runDemo = function(vertShaderSource, fragShaderSource, modelText){
     console.log(modelText);
 
     const canvas = getCanvas("game-surface");
-    const gl = this.getWebglContext(canvas);
+    const gl = getWebglContext(canvas);
 
     gl.clearColor(0.0, 0.5, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT, gl.DEPTH_BUFFER_BIT);
@@ -41,11 +43,6 @@ window.runDemo = function(vertShaderSource, fragShaderSource, modelText){
     gl.enable(gl.CULL_FACE);
     gl.frontFace(gl.CCW);
     gl.cullFace(gl.BACK);
-
-    // //get vertex and fragment shader from html file
-    // //I've also written these in a separate file, but since js can't access locally with a webserver, this is the alt solution.
-    // const vertShaderSource = document.getElementById("vertex-shader").text;
-    // const fragShaderSource = document.getElementById("fragment-shader").text;
 
     //create shaders
     const vertexShader = gl.createShader(gl.VERTEX_SHADER);
@@ -69,30 +66,32 @@ window.runDemo = function(vertShaderSource, fragShaderSource, modelText){
 		return;
 	}
 
-    //create program
-    const program = gl.createProgram();
+  //create program
+  const program = gl.createProgram();
 
-    //attach shaders to the program
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
+  //attach shaders to the program
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
 
-    //link the program
-    gl.linkProgram(program);
-    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+  //link the program
+  gl.linkProgram(program);
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
 		console.error('ERROR linking program!', gl.getProgramInfoLog(program));
 		return;
-	}
-	gl.validateProgram(program);
+  }
+  
+  gl.validateProgram(program);
+  
 	if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
 		console.error('ERROR validating program!', gl.getProgramInfoLog(program));
 		return;
-    }
+  }
     
-    //create the buffer
-    const model = CreateFlowerPot(modelText);
-    let Matrix = new this.Learn_webgl_matrix();
-    const FlowerPot = new FlowerPotRender(gl, program, model, Matrix);
+   //create the buffer
+  const model = CreateFlowerPot(modelText);
+  let Matrix = new Learn_webgl_matrix();
+  const FlowerPot = new FlowerPotRender(gl, program, model, Matrix);
 
-    FlowerPot.render(gl);
+  FlowerPot.render(gl);
 
 }
